681e28d3d42ec233d35996391be054ad3d18e741
mvrv-gi <jossymurugi@gmail.com>
2026-09-11 10:52:29 -0400

    Add basic input/output for name and age

---
+++ basic.py
@@ -0,0 +1,9 @@
+#input and output 
+
+name: str = input ('what is your name?')
+age_string: str = input('How old are you?')
+age: int = int(age_string)
+# age = int(input('how old are you')
+name: str ='kumar'
+age: int = 9
+print(f'In 5 years, (name) will be (age + 5) years old")
